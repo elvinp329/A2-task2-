cdd0820f87cfecb8cbdcd6431b1e5bb9bfa11293
Elvinp329 <elvinkj@code.edu.az>
2023-01-03 00:26:00 +0400

    A2 task2

---
+++ A2 task2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace A2_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int zeroCount = 0;
+            int tmp = n;
+
+            while (tmp != 0)
+            {
+                if (tmp % 10 == 0) zeroCount++;
+                tmp /= 10;
+
+            }
+
+
+            Console.WriteLine($"{n}-deki sifirlari sayi ={zeroCount}");
+        }
+    }
+}
